b37e0d667dde200f168269d279bdd8c2500039b4
fyx <qianfeng@lingchen.local>
2016-05-25 19:35:28 +0800

    1.helloworld

---
+++ HelloGit/HelloGit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloGit
+//
+//  Created by 千锋 on 16/5/25.
+//  Copyright © 2016年 fyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
